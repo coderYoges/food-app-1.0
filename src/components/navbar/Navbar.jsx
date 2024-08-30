03e4dd5922ded1d5555c05c7bc299806f2720275
Rv Yoges <rvyogeswaran@gmail.com>
2024-08-30 23:01:06 +0530

    Feat: Whatsapp icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,6 +8,7 @@ import { MdOutlineAnnouncement } from "react-icons/md";
 import { Modal } from "react-responsive-modal";
 import { IoMdCloseCircle } from "react-icons/io";
 import EnquireCmpt from "../enquiry/Enquiry";
+import { FaWhatsapp } from "react-icons/fa";
 
 const NavbarContainer = styled.nav`
   @media (min-width: 992px) {
@@ -112,6 +113,29 @@ const ModalCloseIcon = styled(IoMdCloseCircle)`
   color: #000;
 `;
 
+const WhatsappWrapper = styled.button`
+  width: 150px;
+  height: 40px;
+  position: fixed;
+  right: 5vw;
+  top: 90vh;
+  z-index: 100;
+  background-color: rgb(37, 211, 102);
+  border-radius: 30px;
+`;
+
+const WhatsappText = styled.span`
+  font-weight: 600;
+  color: #fff;
+`;
+
+const WhatsappIcon = styled(FaWhatsapp)`
+  width: 1.5rem;
+  height: 1.5rem;
+  color: #fff;
+  padding-left: 6px;
+`;
+
 const NavbarCmpt = ({ navbarOpened, setNavbar }) => {
   const [isNavOpened, setNavOpened] = useState(false);
   const [enquiryOpened, setEnquiryOpened] = useState(false);
@@ -126,95 +150,103 @@ const NavbarCmpt = ({ navbarOpened, setNavbar }) => {
 
   const navigate = useNavigate();
   return (
-    <NavbarContainer className="navbar navbar-expand-lg bg-dark px-4 px-lg-5 py-3">
-      <NavbarBrand className="p-0">
-        <NavbarBanner>{headerConstants.title}</NavbarBanner>
-      </NavbarBrand>
-      <div>
-        <NavbarCollapse
-          type="button"
-          className="mx-2"
-          onClick={() => setEnquiryOpened(!enquiryOpened)}
+    <React.Fragment>
+      <NavbarContainer className="navbar navbar-expand-lg bg-dark px-4 px-lg-5 py-3">
+        <NavbarBrand className="p-0">
+          <NavbarBanner>{headerConstants.title}</NavbarBanner>
+        </NavbarBrand>
+        <div>
+          <NavbarCollapse
+            type="button"
+            className="mx-2"
+            onClick={() => setEnquiryOpened(!enquiryOpened)}
+          >
+            <MdOutlineAnnouncement
+              className="text-light"
+              style={{ opacity: "0.55" }}
+            />
+          </NavbarCollapse>
+          <NavbarCollapse
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            onClick={() => setNavOpened(!isNavOpened)}
+          >
+            <span className="fa fa-bars"></span>
+          </NavbarCollapse>
+        </div>
+        <NavbarCollapseSmall
+          className={`roll-height ${navbarOpened ? "d-block" : "d-none"}`}
         >
-          <MdOutlineAnnouncement
-            className="text-light"
-            style={{ opacity: "0.55" }}
-          />
-        </NavbarCollapse>
-        <NavbarCollapse
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-          onClick={() => setNavOpened(!isNavOpened)}
-        >
-          <span className="fa fa-bars"></span>
-        </NavbarCollapse>
-      </div>
-      <NavbarCollapseSmall
-        className={`roll-height ${navbarOpened ? "d-block" : "d-none"}`}
-      >
-        <div
-          className="navbar-nav ms-auto py-0 pe-4"
-          onClick={() => {
-            setNavOpened(false);
-            setNavbar(false);
+          <div
+            className="navbar-nav ms-auto py-0 pe-4"
+            onClick={() => {
+              setNavOpened(false);
+              setNavbar(false);
+            }}
+          >
+            {headerConstants.paths.map((item, index) => (
+              <NavbarLink
+                key={item + index}
+                className="nav-link"
+                onClick={() =>
+                  navigate(`/${item.toLowerCase()}`, { replace: true })
+                }
+                active={window.location.pathname.includes(item.toLowerCase())}
+              >
+                {item}
+              </NavbarLink>
+            ))}
+          </div>
+        </NavbarCollapseSmall>
+        <div className="collapse navbar-collapse">
+          <div
+            className="navbar-nav ms-auto py-0 pe-4"
+            onClick={() => {
+              setNavOpened(false);
+              setNavbar(false);
+            }}
+          >
+            {headerConstants.paths.map((item, index) => (
+              <NavbarLink
+                key={item + index}
+                className="nav-link"
+                onClick={() =>
+                  navigate(`/${item.toLowerCase()}`, { replace: true })
+                }
+                active={window.location.pathname.includes(item.toLowerCase())}
+              >
+                {item}
+              </NavbarLink>
+            ))}
+          </div>
+          <EnquiryLink onClick={() => setEnquiryOpened(!enquiryOpened)}>
+            {headerConstants.link}
+          </EnquiryLink>
+        </div>
+        <Modal
+          open={enquiryOpened}
+          onClose={() => setEnquiryOpened(false)}
+          center
+          closeIcon={<ModalCloseIcon />}
+          styles={{
+            modal: { backgroundColor: "#fff" },
+            overlay: { opacity: "0.5" },
           }}
         >
-          {headerConstants.paths.map((item, index) => (
-            <NavbarLink
-              key={item + index}
-              className="nav-link"
-              onClick={() =>
-                navigate(`/${item.toLowerCase()}`, { replace: true })
-              }
-              active={window.location.pathname.includes(item.toLowerCase())}
-            >
-              {item}
-            </NavbarLink>
-          ))}
-        </div>
-      </NavbarCollapseSmall>
-      <div className="collapse navbar-collapse">
-        <div
-          className="navbar-nav ms-auto py-0 pe-4"
-          onClick={() => {
-            setNavOpened(false);
-            setNavbar(false);
-          }}
-        >
-          {headerConstants.paths.map((item, index) => (
-            <NavbarLink
-              key={item + index}
-              className="nav-link"
-              onClick={() =>
-                navigate(`/${item.toLowerCase()}`, { replace: true })
-              }
-              active={window.location.pathname.includes(item.toLowerCase())}
-            >
-              {item}
-            </NavbarLink>
-          ))}
-        </div>
-        <EnquiryLink onClick={() => setEnquiryOpened(!enquiryOpened)}>
-          {headerConstants.link}
-        </EnquiryLink>
-      </div>
-      <Modal
-        open={enquiryOpened}
-        onClose={() => setEnquiryOpened(false)}
-        center
-        closeIcon={<ModalCloseIcon />}
-        styles={{
-          modal: { backgroundColor: "#fff" },
-          overlay: { opacity: "0.5" },
-        }}
+          <ModalHeader>{enquiryConstants.title}</ModalHeader>
+          <div className="mt-5 ">
+            <EnquireCmpt onClose={() => setEnquiryOpened(false)} />
+          </div>
+        </Modal>
+      </NavbarContainer>
+      <WhatsappWrapper
+        onClick={() => window.open("https://wa.me/919789925091")}
       >
-        <ModalHeader>{enquiryConstants.title}</ModalHeader>
-        <div className="mt-5 ">
-          <EnquireCmpt onClose={() => setEnquiryOpened(false)} />
-        </div>
-      </Modal>
-    </NavbarContainer>
+        <WhatsappText>Whatsapp us</WhatsappText>
+        <WhatsappIcon />
+      </WhatsappWrapper>
+    </React.Fragment>
   );
 };
 
